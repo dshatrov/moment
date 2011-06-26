795504c4cacb15ae9ae30601d64748fc15b7979f
Dmitry Shatrov <shatrov@gmail.com>
2011-06-26 15:56:28 +0400

    RtmpClient class (preparation for multiple connections).

--- moment/rtmptool.cpp
+++ moment/rtmptool.cpp
@@ -11,50 +11,77 @@ using namespace Moment;
 
 namespace {
 
-void connected (Exception *exc_,
-	        void      *_rtmp_conn);
+class RtmpClient : public DependentCodeReferenced
+{
+private:
+    enum ConnectionState {
+	ConnectionState_Connect,
+	ConnectionState_ConnectSent,
+	ConnectionState_CreateStreamSent,
+	ConnectionState_PlaySent,
+	ConnectionState_Streaming
+    };
 
-void closeRtmpConn (void *cb_data);
+    RtmpConnection rtmp_conn;
+    TcpConnection tcp_conn;
+    DeferredConnectionSender conn_sender;
+    ConnectionReceiver conn_receiver;
 
-RtmpConnection::Backend rtmp_conn_backend = {
+    ConnectionState conn_state;
+
+    Uint32 glob_stream_id;
+
+    static TcpConnection::Frontend const tcp_conn_frontend;
+
+    static void connected (Exception *exc_,
+			   void      *_rtmp_conn);
+
+    static RtmpConnection::Backend const rtmp_conn_backend;
+
+    static void closeRtmpConn (void *cb_data);
+
+    static RtmpConnection::Frontend const rtmp_conn_frontend;
+
+    static Result handshakeComplete (void *cb_data);
+
+    static Result commandMessageCallback (RtmpConnection::MessageInfo * mt_nonnull msg_info,
+					  PagePool::PageListHead      * mt_nonnull page_list,
+					  Size                         msg_len,
+					  AmfEncoding                  amf_encoding,
+					  void                        *_self);
+
+    static Result audioMessage (RtmpConnection::MessageInfo * mt_nonnull msg_info,
+				PagePool::PageListHead      * mt_nonnull page_list,
+				Size                         msg_len,
+				void                        *_self);
+
+    static Result videoMessage (RtmpConnection::MessageInfo * mt_nonnull msg_info,
+				PagePool::PageListHead      * mt_nonnull page_list,
+				Size                         msg_len,
+				void                        *_self);
+
+    static void closed (Exception *exc_,
+			void      *_self);
+
+public:
+    Result start (PollGroup       *poll_group,
+		  IpAddress const &addr);
+
+    void init (Timers   *timers,
+	       PagePool *page_pool);
+
+    RtmpClient (Object *coderef_container);
+};
+
+TcpConnection::Frontend const RtmpClient::tcp_conn_frontend = {
+    connected
+};
+
+RtmpConnection::Backend const RtmpClient::rtmp_conn_backend = {
     closeRtmpConn
 };
 
-enum ConnectionState {
-    ConnectionState_Connect,
-    ConnectionState_ConnectSent,
-    ConnectionState_CreateStreamSent,
-    ConnectionState_PlaySent,
-    ConnectionState_Streaming
-};
-ConnectionState conn_state = ConnectionState_Connect;
-
-Uint32 glob_stream_id = 0;
-
-// TODO connect -> createStream -> play
-
-Result handshakeComplete (void *cb_data);
-
-Result commandMessageCallback (RtmpConnection::MessageInfo * mt_nonnull msg_info,
-			       PagePool::PageListHead      * mt_nonnull page_list,
-			       Size                         msg_len,
-			       AmfEncoding                  amf_encoding,
-			       void                        *cb_data);
-
-Result audioMessage (RtmpConnection::MessageInfo * mt_nonnull msg_info,
-		     PagePool::PageListHead      * mt_nonnull page_list,
-		     Size                         msg_len,
-		     void                        *cb_data);
-
-Result videoMessage (RtmpConnection::MessageInfo * mt_nonnull msg_info,
-		     PagePool::PageListHead      * mt_nonnull page_list,
-		     Size                         msg_len,
-		     void                        *cb_data);
-
-void closed (Exception *exc_,
-	     void      *cb_data);
-
-RtmpConnection::Frontend const rtmp_conn_frontend = {
+RtmpConnection::Frontend const RtmpClient::rtmp_conn_frontend = {
     handshakeComplete,
     commandMessageCallback,
     audioMessage,
@@ -62,61 +89,9 @@ RtmpConnection::Frontend const rtmp_conn_frontend = {
     closed
 };
 
-Result doTest (void)
-{
-    MyCpp::myCppInit ();
-    libMaryInit ();
-
-    PagePool page_pool (4096 /* page_size */, 128 /* min_pages */);
-
-    ServerApp server_app (NULL /* coderef_container */);
-    if (!server_app.init ()) {
-	logE_ (_func, "server_app.init() failed: ", exc->toString());
-	return Result::Failure;
-    }
-
-    IpAddress addr;
-    if (!setIpAddress ("localhost:8083", &addr)) {
-	logE_ (_func, "setIpAddress() failed");
-	return Result::Failure;
-    }
-
-    RtmpConnection rtmp_conn (NULL /* coderef_container */, server_app.getTimers(), &page_pool);
-
-    TcpConnection tcp_conn (NULL /* coderef_container */);
-
-    DeferredConnectionSender conn_sender (NULL /* coderef_container */);
-    conn_sender.setConnection (&tcp_conn);
-
-    ConnectionReceiver conn_receiver (NULL /* coderef_container */, &tcp_conn);
-    conn_receiver.setConnection (&tcp_conn);
-    conn_receiver.setFrontend (rtmp_conn.getReceiverFrontend());
-
-    rtmp_conn.setBackend (Cb<RtmpConnection::Backend> (&rtmp_conn_backend, NULL /* cb_data */, NULL /* coderef_container */));
-    rtmp_conn.setFrontend (Cb<RtmpConnection::Frontend> (&rtmp_conn_frontend, &rtmp_conn, NULL /* coderef_container */));
-    rtmp_conn.setSender (&conn_sender);
-
-    TcpConnection::Frontend const tcp_conn_frontend = {
-	connected
-    };
-    tcp_conn.setFrontend (Cb<TcpConnection::Frontend> (&tcp_conn_frontend, &rtmp_conn, NULL /* coderef_container */));
-
-    if (!tcp_conn.connect (addr)) {
-	logE_ (_func, "tcp_conn.connect() failed: ", exc->toString());
-	return Result::Failure;
-    }
-
-    server_app.getPollGroup()->addPollable (tcp_conn.getPollable());
-
-    logD_ (_func, "Starting...");
-    server_app.run ();
-    logD_ (_func, "...Finished");
-
-    return Result::Success;
-}
-
-void connected (Exception * const exc_,
-		void      * const _rtmp_conn)
+void
+RtmpClient::connected (Exception * const exc_,
+		       void      * const _rtmp_conn)
 {
     if (exc_)
 	exit (EXIT_FAILURE);
@@ -127,31 +102,36 @@ void connected (Exception * const exc_,
     rtmp_conn->startClient ();
 }
 
-void closeRtmpConn (void * const /* cb_data */)
+void
+RtmpClient::closeRtmpConn (void * const /* cb_data */)
 {
     logI_ (_func, "Connection closed");
     exit (0);
 }
 
-Result handshakeComplete (void * const _rtmp_conn)
+Result
+RtmpClient::handshakeComplete (void * const _self)
 {
     logD_ (_func_);
 
-    RtmpConnection * const rtmp_conn = static_cast <RtmpConnection*> (_rtmp_conn);
-    rtmp_conn->sendConnect ();
-    conn_state = ConnectionState_ConnectSent;
+    RtmpClient * const self = static_cast <RtmpClient*> (_self);
+
+    self->rtmp_conn.sendConnect ();
+    self->conn_state = ConnectionState_ConnectSent;
+
     return Result::Success;
 }
 
-Result commandMessageCallback (RtmpConnection::MessageInfo * const mt_nonnull msg_info,
-			       PagePool::PageListHead      * const mt_nonnull page_list,
-			       Size                          const msg_len,
-			       AmfEncoding                   const amf_encoding,
-			       void                        * const _rtmp_conn)
+Result
+RtmpClient::commandMessageCallback (RtmpConnection::MessageInfo * const mt_nonnull msg_info,
+				    PagePool::PageListHead      * const mt_nonnull page_list,
+				    Size                          const msg_len,
+				    AmfEncoding                   const /* amf_encoding */,
+				    void                        * const _self)
 {
     logD_ (_func, "ts: ", msg_info->timestamp);
 
-    RtmpConnection * const rtmp_conn = static_cast <RtmpConnection*> (_rtmp_conn);
+    RtmpClient * const self = static_cast <RtmpClient*> (_self);
 
     PagePool::PageListArray pl_array (page_list->first, msg_len);
     AmfDecoder decoder (AmfEncoding::AMF0, &pl_array, msg_len);
@@ -170,10 +150,10 @@ Result commandMessageCallback (RtmpConnection::MessageInfo * const mt_nonnull ms
 
     ConstMemory method (method_name, method_name_len);
     if (!compare (method, "_result")) {
-	switch (conn_state) {
+	switch (self->conn_state) {
 	    case ConnectionState_ConnectSent: {
-		rtmp_conn->sendCreateStream ();
-		conn_state = ConnectionState_CreateStreamSent;
+		self->rtmp_conn.sendCreateStream ();
+		self->conn_state = ConnectionState_CreateStreamSent;
 	    } break;
 	    case ConnectionState_CreateStreamSent: {
 		double stream_id;
@@ -182,11 +162,11 @@ Result commandMessageCallback (RtmpConnection::MessageInfo * const mt_nonnull ms
 		    return Result::Failure;
 		}
 
-		glob_stream_id = lround (stream_id);
+		self->glob_stream_id = lround (stream_id);
 
-		rtmp_conn->sendPlay ("red5StreamDemo");
+		self->rtmp_conn.sendPlay ("red5StreamDemo");
 
-		conn_state = ConnectionState_Streaming;
+		self->conn_state = ConnectionState_Streaming;
 	    } break;
 	    case ConnectionState_PlaySent: {
 		// Unused
@@ -197,7 +177,7 @@ Result commandMessageCallback (RtmpConnection::MessageInfo * const mt_nonnull ms
 	}
     } else
     if (!compare (method, "_error")) {
-	switch (conn_state) {
+	switch (self->conn_state) {
 	    case ConnectionState_ConnectSent:
 	    case ConnectionState_CreateStreamSent:
 	    case ConnectionState_PlaySent: {
@@ -221,30 +201,115 @@ Result commandMessageCallback (RtmpConnection::MessageInfo * const mt_nonnull ms
     return Result::Success;
 }
 
-Result audioMessage (RtmpConnection::MessageInfo * const mt_nonnull msg_info,
-		     PagePool::PageListHead      * const mt_nonnull page_list,
-		     Size                          const msg_len,
-		     void                        * const /* cb_data */)
+Result
+RtmpClient::audioMessage (RtmpConnection::MessageInfo * const mt_nonnull /* msg_info */,
+			  PagePool::PageListHead      * const mt_nonnull /* page_list */,
+			  Size                          const /* msg_len */,
+			  void                        * const /* _self */)
 {
 //    logD_ (_func, "ts: ", msg_info->timestamp);
     return Result::Success;
 }
 
-Result videoMessage (RtmpConnection::MessageInfo * const mt_nonnull msg_info,
-		     PagePool::PageListHead      * const mt_nonnull page_list,
-		     Size                          const msg_len,
-		     void                        * const /* cb_data */)
+Result
+RtmpClient::videoMessage (RtmpConnection::MessageInfo * const mt_nonnull /* msg_info */,
+			  PagePool::PageListHead      * const mt_nonnull /* page_list */,
+			  Size                          const /* msg_len */,
+			  void                        * const /* _self */)
 {
 //    logD_ (_func, "ts: ", msg_info->timestamp);
-    logs->print (".");
-    logs->flush ();
+
+    static int debug_counter = 10;
+    ++debug_counter;
+    if (debug_counter >= 10) {
+	debug_counter = 0;
+
+	logs->print (".");
+	logs->flush ();
+    }
+
     return Result::Success;
 }
 
-void closed (Exception * const exc_,
-	     void      * const /* cb_data */)
+void
+RtmpClient::closed (Exception * const exc_,
+		    void      * const /* _self */)
 {
-    logD_ (_func_);
+    if (exc_)
+	logD_ (_func, exc_->toString());
+    else
+	logD_ (_func_);
+}
+
+Result
+RtmpClient::start (PollGroup * const  poll_group,
+		   IpAddress   const &addr)
+{
+    if (!tcp_conn.connect (addr)) {
+	logE_ (_func, "tcp_conn.connect() failed: ", exc->toString());
+	return Result::Failure;
+    }
+
+    poll_group->addPollable (tcp_conn.getPollable());
+    return Result::Success;
+}
+
+void
+RtmpClient::init (Timers   * const timers,
+		  PagePool * const page_pool)
+{
+    rtmp_conn.init (timers, page_pool);
+}
+
+RtmpClient::RtmpClient (Object * const coderef_container)
+    : DependentCodeReferenced (coderef_container),
+      rtmp_conn     (coderef_container),
+      tcp_conn      (coderef_container),
+      conn_sender   (coderef_container),
+      conn_receiver (coderef_container),
+      conn_state (ConnectionState_Connect),
+      glob_stream_id (0)
+{
+    conn_sender.setConnection (&tcp_conn);
+
+    conn_receiver.setConnection (&tcp_conn);
+    conn_receiver.setFrontend (rtmp_conn.getReceiverFrontend());
+
+    rtmp_conn.setBackend (Cb<RtmpConnection::Backend> (&rtmp_conn_backend, NULL /* cb_data */, NULL /* coderef_container */));
+    rtmp_conn.setFrontend (Cb<RtmpConnection::Frontend> (&rtmp_conn_frontend, this, getCoderefContainer()));
+    rtmp_conn.setSender (&conn_sender);
+
+    tcp_conn.setFrontend (Cb<TcpConnection::Frontend> (&tcp_conn_frontend, &rtmp_conn, rtmp_conn.getCoderefContainer()));
+}
+
+Result doTest (void)
+{
+    MyCpp::myCppInit ();
+    libMaryInit ();
+
+    PagePool page_pool (4096 /* page_size */, 128 /* min_pages */);
+
+    ServerApp server_app (NULL /* coderef_container */);
+    if (!server_app.init ()) {
+	logE_ (_func, "server_app.init() failed: ", exc->toString());
+	return Result::Failure;
+    }
+
+    IpAddress addr;
+    if (!setIpAddress ("localhost:8083", &addr)) {
+	logE_ (_func, "setIpAddress() failed");
+	return Result::Failure;
+    }
+
+    RtmpClient client (NULL /* coderef_container */);
+    client.init (server_app.getTimers(), &page_pool);
+    client.start (server_app.getPollGroup(), addr);
+
+    logD_ (_func, "Starting...");
+    server_app.run ();
+    logD_ (_func, "...Finished");
+
+    return Result::Success;
 }
 
 } // namespace {}
