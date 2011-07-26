ddd4d2762781a4d3dcf6a3ac1f89463acda50f40
Dmitry Shatrov <shatrov@gmail.com>
2011-07-26 12:37:20 +0400

    upload script update

--- www/upload.sh
+++ www/upload.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# Note that scp doens't work correctly if the new file is smaller than the older one.
+
 #scp -r -P 2222 err momentvideoorg@momentvideo.org:
 #scp -r -P 2222 img momentvideoorg@momentvideo.org:
-scp -r -P 2222 *.html *.xml *.xsl *.svg Makefile img momentvideoorg@momentvideo.org:
+scp -r -P 2222 *.html *.php *.xml *.xsl *.svg Makefile img momentvideoorg@momentvideo.org:
+#scp -P 2222 index.php test.html momentvideoorg@momentvideo.org:
 
