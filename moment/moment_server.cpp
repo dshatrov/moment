12b64e03969bede5c47d7093944860624ca97797
Dmitry Shatrov <shatrov@gmail.com>
2011-08-29 13:54:53 +0400

    Client sessions; client events; client handlers; client handler namespaces.

--- moment/moment_server.cpp
+++ moment/moment_server.cpp
@@ -26,6 +26,185 @@ using namespace M;
 
 MomentServer* MomentServer::instance = NULL;
 
+namespace {
+    class InformRtmpCommandMessage_Data {
+    public:
+	RtmpConnection           * const conn;
+	VideoStream::MessageInfo * const msg_info;
+	ConstMemory const        &method_name;
+	AmfDecoder               * const amf_decoder;
+
+	InformRtmpCommandMessage_Data (RtmpConnection           * const conn,
+				       VideoStream::MessageInfo * const msg_info,
+				       ConstMemory const        &method_name,
+				       AmfDecoder               * const amf_decoder)
+	    : conn (conn),
+	      msg_info (msg_info),
+	      method_name (method_name),
+	      amf_decoder (amf_decoder)
+	{
+	}
+    };
+}
+
+void
+MomentServer::ClientSession::informRtmpCommandMessage (Events * const events,
+						       void   * const cb_data,
+						       void   * const _inform_data)
+{
+    InformRtmpCommandMessage_Data * const inform_data =
+	    static_cast <InformRtmpCommandMessage_Data*> (_inform_data);
+    events->rtmpCommandMessage (inform_data->conn,
+				inform_data->msg_info,
+				inform_data->method_name,
+				inform_data->amf_decoder,
+				cb_data);
+}
+
+void
+MomentServer::ClientSession::informClientDisconnected (Events * const events,
+						       void   * const cb_data,
+						       void   * const /* _inform_data */)
+{
+    events->clientDisconnected (cb_data);
+}
+
+bool
+MomentServer::ClientSession::isConnected_subscribe (CbDesc<Events> const &cb)
+{
+    mutex.lock ();
+    if (disconnected) {
+	mutex.unlock ();
+	return false;
+    }
+    event_informer.subscribe_unlocked (cb);
+    mutex.unlock ();
+    return true;
+}
+
+void
+MomentServer::ClientSession::setBackend (CbDesc<Backend> const &cb)
+{
+    backend = cb;
+}
+
+void
+MomentServer::ClientSession::fireRtmpCommandMessage (RtmpConnection           * const mt_nonnull conn,
+						     VideoStream::MessageInfo * const mt_nonnull msg_info,
+						     ConstMemory        const &method_name,
+						     AmfDecoder               * const mt_nonnull amf_decoder)
+{
+    InformRtmpCommandMessage_Data inform_data (conn, msg_info, method_name, amf_decoder);
+    event_informer.informAll (informRtmpCommandMessage, &inform_data);
+}
+
+void
+MomentServer::ClientSession::clientDisconnected ()
+{
+    mutex.lock ();
+    disconnected = true;
+    // To avoid races, we defer invocation of "client disconnected" callbacks
+    // until all "client connected" callbacks are called.
+    if (!processing_connected_event)
+	event_informer.informAll_unlocked (informClientDisconnected, NULL /* inform_data */);
+    mutex.unlock ();
+}
+
+MomentServer::ClientSession::ClientSession ()
+    : disconnected (false),
+      event_informer (this, &mutex)
+{
+    logD_ (_func, "0x", fmt_hex, (UintPtr) this);
+}
+
+MomentServer::ClientSession::~ClientSession ()
+{
+    logD_ (_func, "0x", fmt_hex, (UintPtr) this);
+}
+
+namespace {
+    class InformClientConnected_Data {
+    public:
+	MomentServer::ClientSession * const client_session;
+	ConstMemory const &app_name;
+	ConstMemory const &full_app_name;
+
+	InformClientConnected_Data (MomentServer::ClientSession * const client_session,
+				    ConstMemory const &app_name,
+				    ConstMemory const &full_app_name)
+	    : client_session (client_session),
+	      app_name (app_name),
+	      full_app_name (full_app_name)
+	{
+	}
+    };
+}
+
+void
+MomentServer::ClientEntry::informClientConnected (ClientHandler * const client_handler,
+						  void          * const cb_data,
+						  void          * const _inform_data)
+{
+    InformClientConnected_Data * const inform_data =
+	    static_cast <InformClientConnected_Data*> (_inform_data);
+    client_handler->clientConnected (inform_data->client_session,
+				     inform_data->app_name,
+				     inform_data->full_app_name,
+				     cb_data);
+}
+
+void
+MomentServer::ClientEntry::fireClientConnected (ClientSession     * const client_session,
+						ConstMemory const &app_name,
+						ConstMemory const &full_app_name)
+{
+    InformClientConnected_Data inform_data (client_session, app_name, full_app_name);
+    event_informer.informAll (informClientConnected, &inform_data);
+}
+
+// @ret_path_tail should contain original path.
+mt_mutex (mutex) MomentServer::ClientEntry*
+MomentServer::getClientEntry_rec (ConstMemory   const path,
+				  ConstMemory * const mt_nonnull ret_path_tail,
+				  Namespace   * const mt_nonnull nsp)
+{
+    Byte const *delim = (Byte const *) memchr (path.mem(), '/', path.len());
+    ConstMemory next_name;
+    if (delim) {
+	next_name = path.region (0, delim - path.mem());
+	Namespace::NamespaceHash::EntryKey const next_nsp_key = nsp->namespace_hash.lookup (next_name);
+	if (next_nsp_key) {
+	    Namespace * const next_nsp = next_nsp_key.getData();
+	    ClientEntry * const client_entry = getClientEntry_rec (path.region (delim - path.mem() + 1),
+								   ret_path_tail,
+								   next_nsp);
+	    if (client_entry)
+		return client_entry;
+	}
+    } else {
+	next_name = path;
+    }
+
+    Namespace::ClientEntryHash::EntryKey const client_entry_key = nsp->client_entry_hash.lookup (next_name);
+    if (!client_entry_key)
+	return NULL;
+
+    *ret_path_tail = (*ret_path_tail).region ((next_name.mem() + next_name.len()) - (*ret_path_tail).mem());
+    return client_entry_key.getData();
+}
+
+mt_mutex (mutex) MomentServer::ClientEntry*
+MomentServer::getClientEntry (ConstMemory  path,
+			      ConstMemory * const mt_nonnull ret_path_tail,
+			      Namespace   * const mt_nonnull nsp)
+{
+    if (path.len() > 0 && path.mem() [0] == '/')
+	path = path.region (1);
+
+    *ret_path_tail = path;
+    return getClientEntry_rec (path, ret_path_tail, nsp);
+}
+
 mt_throws Result
 MomentServer::loadModules ()
 {
@@ -122,12 +301,239 @@ MomentServer::getInstance ()
     return instance;
 }
 
+Ref<MomentServer::ClientSession>
+MomentServer::rtmpClientConnected (ConstMemory const &path,
+				   RtmpConnection    * const mt_nonnull conn)
+{
+    logD_ (_func_);
+
+    ConstMemory path_tail;
+
+    mutex.lock ();
+    Ref<ClientEntry> const client_entry = getClientEntry (path, &path_tail, &root_namespace);
+#if 0
+// TODO 'free_access' config option.
+    if (!client_entry) {
+    }
+#endif
+
+    Ref<ClientSession> const client_session = grab (new ClientSession);
+    client_session->weak_rtmp_conn = conn;
+    client_session->unsafe_rtmp_conn = conn;
+    client_session->processing_connected_event = true;
+
+    client_session_list.append (client_session);
+    client_session->ref ();
+    mutex.unlock ();
+
+    logD_ (_func, "client_session refcount before: ", client_session->getRefCount());
+    if (client_entry)
+	client_entry->fireClientConnected (client_session, path_tail, path);
+
+    client_session->mutex.lock ();
+    client_session->processing_connected_event = false;
+    if (client_session->disconnected) {
+	client_session->event_informer.informAll_unlocked (client_session->informClientDisconnected, NULL /* inform_data */);
+	client_session->mutex.unlock ();
+	return NULL;
+    }
+    client_session->mutex.unlock ();
+
+    logD_ (_func, "client_session refcount after: ", client_session->getRefCount());
+    return client_session;
+}
+
+void
+MomentServer::clientDisconnected (ClientSession * const mt_nonnull client_session)
+{
+    logD_ (_func, "client_session refcount before: ", client_session->getRefCount());
+
+    client_session->clientDisconnected ();
+
+    mutex.lock ();
+    if (client_session->video_stream_key)
+	removeVideoStream_unlocked (client_session->video_stream_key);
+
+    client_session_list.remove (client_session);
+    mutex.unlock ();
+
+    logD_ (_func, "client_session refcount after: ", client_session->getRefCount());
+    client_session->unref ();
+}
+
+void
+MomentServer::disconnect (ClientSession * const mt_nonnull client_session)
+{
+    CodeRef rtmp_conn_ref;
+    RtmpConnection * const rtmp_conn = client_session->getRtmpConnection (&rtmp_conn_ref);
+    if (!rtmp_conn)
+	return;
+
+    rtmp_conn->closeAfterFlush ();
+}
+
+Ref<VideoStream>
+MomentServer::startWatching (ClientSession * const mt_nonnull client_session,
+			     ConstMemory const stream_name)
+{
+    if (client_session->backend
+	&& client_session->backend->startWatching)
+    {
+	logD_ (_func, "calling backend->startWatching()");
+	Ref<VideoStream> video_stream;
+	if (!client_session->backend.call_ret< Ref<VideoStream> > (&video_stream,
+								   client_session->backend->startWatching,
+								   /* ( */ stream_name /* ) */))
+	{
+	    return NULL;
+	}
+
+	return video_stream;
+    }
+
+    logD_ (_func, "default path");
+
+    return getVideoStream (stream_name);
+}
+
+Ref<VideoStream>
+MomentServer::startStreaming (ClientSession * const mt_nonnull client_session,
+			      ConstMemory const stream_name)
+{
+    if (client_session->backend
+	&& client_session->backend->startWatching)
+    {
+	logD_ (_func, "calling backend->startStreaming()");
+	Ref<VideoStream> video_stream;
+	if (!client_session->backend.call_ret< Ref<VideoStream> > (&video_stream,
+								   client_session->backend->startStreaming,
+								   /* ( */ stream_name /* ) */))
+	{
+	    return NULL;
+	}
+
+	return video_stream;
+    }
+
+    logD_ (_func, "default path");
+
+    Ref<VideoStream> const video_stream = grab (new VideoStream);
+    VideoStreamKey const video_stream_key = addVideoStream (video_stream, stream_name);
+
+    client_session->mutex.lock ();
+    if (client_session->disconnected) {
+	client_session->mutex.unlock ();
+	removeVideoStream (video_stream_key);
+	return NULL;
+    }
+    client_session->video_stream_key = video_stream_key;
+    client_session->mutex.unlock ();
+
+    return video_stream;
+}
+
+mt_mutex (mutex) MomentServer::ClientHandlerKey
+MomentServer::addClientHandler_rec (CbDesc<ClientHandler> const &cb,
+				    ConstMemory const           &path_,
+				    Namespace                   * const nsp)
+{
+  // This code is pretty much like M::HttpService::addHttpHandler_rec()
+
+    ConstMemory path = path_;
+    if (path.len() > 0 && path.mem() [0] == '/')
+	path = path.region (1);
+
+    Byte const *delim = (Byte const *) memchr (path.mem(), '/', path.len());
+    if (!delim) {
+	ClientEntry *client_entry;
+	{
+	    Namespace::ClientEntryHash::EntryKey client_entry_key = nsp->client_entry_hash.lookup (path);
+	    if (!client_entry_key) {
+		Ref<ClientEntry> new_entry = grab (new ClientEntry);
+		new_entry->parent_nsp = nsp;
+		new_entry->client_entry_key = nsp->client_entry_hash.add (path, new_entry);
+		client_entry = new_entry;
+	    } else {
+		client_entry = client_entry_key.getData();
+	    }
+	}
+
+	ClientHandlerKey client_handler_key;
+	client_handler_key.client_entry = client_entry;
+	client_handler_key.sbn_key = client_entry->event_informer.subscribe (cb);
+	return client_handler_key;
+    }
+
+    ConstMemory const next_nsp_name = path.region (0, delim - path.mem());
+    Namespace::NamespaceHash::EntryKey const next_nsp_key =
+	    nsp->namespace_hash.lookup (next_nsp_name);
+    Namespace *next_nsp;
+    if (next_nsp_key) {
+	next_nsp = next_nsp_key.getData();
+    } else {
+	Ref<Namespace> const new_nsp = grab (new Namespace);
+	new_nsp->parent_nsp = nsp;
+	new_nsp->namespace_hash_key = nsp->namespace_hash.add (next_nsp_name, new_nsp);
+	next_nsp = new_nsp;
+    }
+
+    return addClientHandler_rec (cb, path.region (delim - path.mem() + 1), next_nsp);
+}
+
+MomentServer::ClientHandlerKey
+MomentServer::addClientHandler (CbDesc<ClientHandler> const &cb,
+				ConstMemory           const &path)
+{
+    mutex.lock ();
+    ClientHandlerKey const client_handler_key = addClientHandler_rec (cb, path, &root_namespace);
+    mutex.unlock ();
+    return client_handler_key;
+}
+
+void
+MomentServer::removeClientHandler (ClientHandlerKey const client_handler_key)
+{
+    mutex.lock ();
+
+    Ref<ClientEntry> const client_entry = client_handler_key.client_entry;
+
+    bool remove_client_entry = false;
+    client_entry->mutex.lock ();
+    client_entry->event_informer.unsubscribe (client_handler_key.sbn_key);
+    if (!client_entry->event_informer.gotSubscriptions_unlocked ()) {
+	remove_client_entry = true;
+    }
+    client_entry->mutex.unlock ();
+
+    if (remove_client_entry) {
+	client_entry->parent_nsp->client_entry_hash.remove (client_entry->client_entry_key);
+
+	{
+	    Namespace *nsp = client_entry->parent_nsp;
+	    while (nsp && nsp->parent_nsp) {
+		if (nsp->client_entry_hash.isEmpty() &&
+		    nsp->namespace_hash.isEmpty())
+		{
+		    Namespace * const tmp_nsp = nsp;
+		    nsp = nsp->parent_nsp;
+		    nsp->namespace_hash.remove (tmp_nsp->namespace_hash_key);
+		}
+	    }
+	}
+    }
+
+    mutex.unlock ();
+}
+
+#if 0
+// Unused
 void
 MomentServer::addVideoStreamHandler (Cb<VideoStreamHandler> const &cb,
 				     ConstMemory const &path_prefix)
 {
   // TODO
 }
+#endif
 
 Ref<VideoStream>
 MomentServer::getVideoStream (ConstMemory const &path)
@@ -149,11 +555,17 @@ MomentServer::addVideoStream (VideoStream * const  video_stream,
     return video_stream_hash.add (path, video_stream);
 }
 
+mt_mutex (mutex) void
+MomentServer::removeVideoStream_unlocked (VideoStreamKey const video_stream_key)
+{
+    video_stream_hash.remove (video_stream_key.entry_key);
+}
+
 void
 MomentServer::removeVideoStream (VideoStreamKey const video_stream_key)
 {
     mutex.lock ();
-    video_stream_hash.remove (video_stream_key.entry_key);
+    removeVideoStream_unlocked (video_stream_key);
     mutex.unlock ();
 }
 
--- moment/moment_server.h
+++ moment/moment_server.h
@@ -37,21 +37,7 @@ using namespace M;
 // This limitation comes form loadable modules support.
 class MomentServer
 {
-public:
-    // TODO Unused
-    struct VideoStreamHandler
-    {
-	Result (*videoStreamOpened) (VideoStream * mt_nonnull video_stream);
-    };
-
 private:
-    ServerApp *server_app;
-    PagePool *page_pool;
-    HttpService *http_service;
-    MConfig::Config *config;
-
-    static MomentServer *instance;
-
     class VideoStreamEntry
     {
     public:
@@ -64,9 +50,203 @@ private:
     };
 
     typedef StringHash<VideoStreamEntry> VideoStreamHash;
-    VideoStreamHash video_stream_hash;
+
+public:
+    class VideoStreamKey
+    {
+	friend class MomentServer;
+    private:
+	VideoStreamHash::EntryKey entry_key;
+	VideoStreamKey (VideoStreamHash::EntryKey entry_key) : entry_key (entry_key) {}
+    public:
+	operator bool () const { return entry_key; }
+	VideoStreamKey () {}
+
+	// Methods for C API binding.
+	void *getAsVoidPtr () const { return entry_key.getAsVoidPtr(); }
+	static VideoStreamKey fromVoidPtr (void *ptr) {
+		return VideoStreamKey (VideoStreamHash::EntryKey::fromVoidPtr (ptr)); }
+    };
+
+#if 0
+    // TODO Unused
+    struct VideoStreamHandler
+    {
+	Result (*videoStreamOpened) (VideoStream * mt_nonnull video_stream);
+    };
+#endif
+
+    class ClientSessionList_name;
+
+    class ClientSession : public Object,
+			  public IntrusiveListElement<ClientSessionList_name>
+    {
+	friend class MomentServer;
+
+    public:
+	struct Events
+	{
+	    void (*rtmpCommandMessage) (RtmpConnection           * mt_nonnull conn,
+					VideoStream::MessageInfo * mt_nonnull msg_info,
+					ConstMemory const        &method_name,
+					AmfDecoder               * mt_nonnull amf_decoder,
+					void                     *cb_data);
+
+	    void (*clientDisconnected) (void *cb_data);
+	};
+
+	struct Backend
+	{
+	    Ref<VideoStream> (*startWatching) (ConstMemory  stream_name,
+					       void        *cb_data);
+
+	    Ref<VideoStream> (*startStreaming) (ConstMemory  stream_name,
+						void        *cb_data);
+	};
+
+    private:
+	mt_mutex (mutex) bool processing_connected_event;
+	mt_mutex (mutex) bool disconnected;
+
+	mt_const WeakCodeRef weak_rtmp_conn;
+	mt_const RtmpConnection *unsafe_rtmp_conn;
+
+	Informer_<Events> event_informer;
+	Cb<Backend> backend;
+
+	// TODO synchronization - ?
+	VideoStreamKey video_stream_key;
+
+	static void informRtmpCommandMessage (Events *events,
+					      void   *cb_data,
+					      void   *inform_data);
+
+	static void informClientDisconnected (Events *events,
+					      void   *cb_data,
+					      void   *inform_data);
+
+    public:
+	Informer_<Events>* getEventInformer ()
+	{
+	    return &event_informer;
+	}
+
+	bool isConnected_subscribe (CbDesc<Events> const &cb);
+
+	// Should be called by clientConnected handlers only.
+	void setBackend (CbDesc<Backend> const &cb);
+
+    private:
+	void fireRtmpCommandMessage (RtmpConnection           * mt_nonnull conn,
+				     VideoStream::MessageInfo * mt_nonnull msg_info,
+				     ConstMemory        const &method_name,
+				     AmfDecoder               * mt_nonnull amf_decoder);
+
+	void clientDisconnected ();
+
+	ClientSession ();
+
+    public:
+	RtmpConnection* getRtmpConnection (CodeRef * const mt_nonnull ret_code_ref)
+	{
+	    *ret_code_ref = weak_rtmp_conn;
+	    if (*ret_code_ref)
+		return unsafe_rtmp_conn;
+
+	    return NULL;
+	}
+
+	~ClientSession ();
+    };
+
+    typedef IntrusiveList<ClientSession, ClientSessionList_name> ClientSessionList;
+
+    struct ClientHandler
+    {
+	void (*clientConnected) (ClientSession     *client_session,
+				 ConstMemory const &app_name,
+				 ConstMemory const &full_app_name,
+				 void              *cb_data);
+    };
+
+private:
+    class Namespace;
+
+public:
+    class ClientEntry : public Object
+    {
+	friend class MomentServer;
+
+    private:
+	Namespace *parent_nsp;
+	StringHash< Ref<ClientEntry> >::EntryKey client_entry_key;
+
+	Informer_<ClientHandler> event_informer;
+
+	static void informClientConnected (ClientHandler *client_handler,
+					   void          *cb_data,
+					   void          *inform_data);
+
+	void fireClientConnected (ClientSession     *client_session,
+				  ConstMemory const &app_name,
+				  ConstMemory const &full_app_name);
+
+    public:
+	Informer_<ClientHandler>* getEventInformer ()
+	{
+	    return &event_informer;
+	}
+
+	ClientEntry ()
+	    : event_informer (this, &mutex)
+	{
+	}
+    };
+
+private:
+    mt_const ServerApp *server_app;
+    mt_const PagePool *page_pool;
+    mt_const HttpService *http_service;
+    mt_const MConfig::Config *config;
+
+    mt_mutex (mutex) ClientSessionList client_session_list;
+
+    static MomentServer *instance;
+
+    mt_mutex (mutex) VideoStreamHash video_stream_hash;
+
+    class Namespace : public BasicReferenced
+    {
+    public:
+	// We use Ref<Namespace> because 'Namespace' is an incomplete type here
+	// (language limitation).
+	typedef StringHash< Ref<Namespace> > NamespaceHash;
+	// We use Ref<ClientEntry> because of ClientEntry::event_informer.
+	typedef StringHash< Ref<ClientEntry> > ClientEntryHash;
+
+	Namespace *parent_nsp;
+	NamespaceHash::EntryKey namespace_hash_key;
+
+	NamespaceHash namespace_hash;
+	ClientEntryHash client_entry_hash;
+
+	Namespace ()
+	    : parent_nsp (NULL)
+	{
+	}
+    };
+
+    mt_mutex (mutex) Namespace root_namespace;
 
     StateMutex mutex;
+
+    mt_mutex (mutex) ClientEntry* getClientEntry_rec (ConstMemory  path,
+						      ConstMemory * mt_nonnull ret_path_tail,
+						      Namespace   * mt_nonnull nsp);
+
+    mt_mutex (mutex) ClientEntry* getClientEntry (ConstMemory  path,
+						  ConstMemory * mt_nonnull ret_path_tail,
+						  Namespace   * mt_nonnull nsp);
 
     mt_throws Result loadModules ();
 
@@ -83,29 +263,59 @@ public:
 
     static MomentServer* getInstance ();
 
+  // Client events
+
+    Ref<ClientSession> rtmpClientConnected (ConstMemory const &path,
+					    RtmpConnection    * mt_nonnull conn);
+
+    void clientDisconnected (ClientSession * mt_nonnull client_session);
+
+    void disconnect (ClientSession * mt_nonnull client_session);
+
+    Ref<VideoStream> startWatching (ClientSession * mt_nonnull client_session,
+				    ConstMemory    stream_name);
+
+    Ref<VideoStream> startStreaming (ClientSession * mt_nonnull client_session,
+				     ConstMemory    stream_name);
+
+    struct ClientHandlerKey {
+	ClientEntry *client_entry;
+	GenericInformer::SubscriptionKey sbn_key;
+    };
+
+private:
+    mt_mutex (mutex) ClientHandlerKey addClientHandler_rec (CbDesc<ClientHandler> const &cb,
+							    ConstMemory const           &path,
+							    Namespace                   *nsp);
+
+public:
+    ClientHandlerKey addClientHandler (CbDesc<ClientHandler> const &cb,
+				       ConstMemory           const &path);
+
+    void removeClientHandler (ClientHandlerKey client_handler_key);
+
   // Video stream handlers
 
+#if 0
+// Unused
     void addVideoStreamHandler (Cb<VideoStreamHandler> const &cb,
 				ConstMemory const &path_prefix);
+#endif
 
   // Get/add/remove video streams
 
-    class VideoStreamKey
-    {
-	friend class MomentServer;
-    private:
-	VideoStreamHash::EntryKey entry_key;
-	VideoStreamKey (VideoStreamHash::EntryKey entry_key) : entry_key (entry_key) {}
-    public:
-	operator bool () const { return entry_key; }
-	VideoStreamKey () {}
-    };
-
+    // TODO There's a logical problem here. A stream can only be deleted
+    // by the one who created it. This limitation makes little sense.
+    // But overcoming it requires more complex synchronization.
     Ref<VideoStream> getVideoStream (ConstMemory const &path);
 
     VideoStreamKey addVideoStream (VideoStream * const video_stream,
 				   ConstMemory const &path);
 
+private:
+    mt_mutex (mutex) void removeVideoStream_unlocked (VideoStreamKey video_stream_key);
+
+public:
     void removeVideoStream (VideoStreamKey video_stream_key);
 
   // Initialization
