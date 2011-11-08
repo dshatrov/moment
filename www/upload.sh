622ca482d9518fbfca49271be3f09945e0082d64
Dmitry Shatrov <shatrov@gmail.com>
2011-11-09 01:09:51 +0400

    upload.sh update

--- www/upload.sh
+++ www/upload.sh
@@ -4,6 +4,6 @@
 
 #scp -r -P 2222 err momentvideoorg@momentvideo.org:
 #scp -r -P 2222 img momentvideoorg@momentvideo.org:
-scp -r -P 2222 *.html *.php *.xml *.xsl *.svg Makefile img momentvideoorg@momentvideo.org:
+scp -r -P 2222 *.html *.php *.xml *.xsl *.svg *.ico Makefile img momentvideoorg@momentvideo.org:
 #scp -P 2222 index.php test.html momentvideoorg@momentvideo.org:
 
